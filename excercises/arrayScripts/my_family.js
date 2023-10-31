a5309a7efb479fb33e44d980a83033f2cd438423
J Gdovic <jgdovic3@gmail.com>
2023-10-31 14:26:10 -0400

    my_family EX 1

---
+++ excercises/arrayScripts/my_family.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let myFamily = ["Javier","Jordan","Jules","Aleena"]
+
+for (let index = 0; index < myFamily.length; index++) {
+    const element = myFamily[index];
+    console.log(element);
+    
+}
