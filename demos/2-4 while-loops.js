7dcfeeee9d034014def826a10c897bf3c20e4f6a
J Gdovic <jgdovic3@gmail.com>
2023-10-31 10:48:24 -0400

    add comments

--- demos/2-4 while-loops.js
+++ demos/2-4 while-loops.js
@@ -1,8 +1,11 @@
 "use strict";
 
-
+// this variable keeps track of the number of times the loop runs
 let count = 1;
 while (count <= 10) {
+    //stuff in here is going to happen 10 times
   console.log("did action");
-  count = count+ 1;
+  count = count+ 1;// incrementing the loop so it dosent run infinitely
 }
+
+
