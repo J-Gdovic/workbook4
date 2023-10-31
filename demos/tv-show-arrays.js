9fbbcb3292dc546324b6636e713e63ba0759e231
J Gdovic <jgdovic3@gmail.com>
2023-10-31 13:39:46 -0400

    using for loops with arrays

--- demos/tv-show-arrays.js
+++ demos/tv-show-arrays.js
@@ -1,7 +1,14 @@
 "use strict"
 
 let tvShows = ["Breaking Bad","Better Call Saul","Friends","Supernatural"]
-console.log(tvShows[0]);
-console.log(tvShows[1]);
-console.log(tvShows[2]);
-console.log(tvShows[3]);
+//writing for loop using array
+for(let count = 0; count<4; count++){
+    console.log(tvShows[count])
+}
+//^^ is the shorter version using a for loop
+
+//below is using an array with subsstring syntax.
+// console.log(tvShows[0]);
+// console.log(tvShows[1]);
+// console.log(tvShows[2]);
+// console.log(tvShows[3]);
