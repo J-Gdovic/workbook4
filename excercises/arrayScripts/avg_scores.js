895b8de67e7598cfbbcc612f67fc6059f5257839
J Gdovic <jgdovic3@gmail.com>
2023-10-31 15:23:08 -0400

    found myScore and yourScore total

---
+++ excercises/arrayScripts/avg_scores.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// function getAverage() {
+//   for (let index = 0; index <= 7; index++) {
+//     console.log(myScores[index]);
+//   }
+// }
+// getAverage();
+
+let myScores = [92, 98, 84, 76, 89, 99, 100];
+let yourScores = [82, 98, 94, 88, 92, 100, 100];
+
+function getAverage(scores) {
+  let total = 0;
+  for (let index = 0; index < scores.length; index++) {
+    const score = scores[index];
+    //total = total + score; or:
+    total += score;
+  }
+  console.log(total);
+}
+getAverage(myScores);
+getAverage(yourScores);
