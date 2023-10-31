a9832c2e8ef7d896fb63e89573f4dde0824c4d52
J Gdovic <jgdovic3@gmail.com>
2023-10-31 11:26:17 -0400

    for loop example

---
+++ demos/2-6-for-loop.js
@@ -0,0 +1,17 @@
+"use strict";
+// part 1: code that executes before the loop starts
+// let count = 1;
+// // part 2 : condition that must be true for the loop to continue
+// while(count<=100){
+//     console.log("this is a example of a for loop")
+//     //part 3: code that runs at the bottom of each iteration
+//     count = count +1;
+// }
+
+// for(part1; part2; part3){
+
+// }
+
+for(let count = 1; count<=10; count = count +1){
+    console.log("this is a example of a for loop")
+}
