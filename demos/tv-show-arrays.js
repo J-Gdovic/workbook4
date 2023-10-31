6444248fb75443f36afe1a970130d0ba0c9f79b3
J Gdovic <jgdovic3@gmail.com>
2023-10-31 14:13:51 -0400

    for loop short cut with arrays

--- demos/tv-show-arrays.js
+++ demos/tv-show-arrays.js
@@ -1,11 +1,23 @@
-"use strict"
+"use strict";
 
-let tvShows = ["Breaking Bad","Better Call Saul","Friends","Supernatural"]
-//writing for loop using array
-for(let count = 0; count<4; count++){
-    console.log(tvShows[count])
+let tvShows = ["Breaking Bad", "Better Call Saul", "Friends", "Supernatural", "Brooklyn 99", "Modern Family"];
+
+
+for (let index = 0; index < tvShows.length; index++) {
+    const element = tvShows[index];
+    console.log(element);
 }
-//^^ is the shorter version using a for loop
+
+
+
+
+
+// //writing for loop using array
+// for (let count = 0; count < tvShows.length; count++) {
+//   console.log(tvShows[count]);
+// }
+//^^ is the shorter version using a for loop with array
+
 
 //below is using an array with subsstring syntax.
 // console.log(tvShows[0]);
