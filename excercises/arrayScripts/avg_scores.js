0b44f9c08a2cd2fb9b433743a53f84784c52bf54
J Gdovic <jgdovic3@gmail.com>
2023-11-01 09:19:25 -0400

    average score exersice

--- excercises/arrayScripts/avg_scores.js
+++ excercises/arrayScripts/avg_scores.js
@@ -1,26 +1,21 @@
 "use strict";
 
-// function getAverage() {
-//   for (let index = 0; index <= 7; index++) {
-//     console.log(myScores[index]);
-//   }
-// }
-// getAverage();
-
 let myScores = [92, 98, 84, 76, 89, 99, 100];
 let yourScores = [82, 98, 94, 88, 92, 100, 100];
 
-function getAverage(scores, name) {
+function getAverage(scores) {
   let total = 0;
+
   for (let index = 0; index < scores.length; index++) {
     const score = scores[index];
     //total = total + score; or:
     total += score;
-    return total / 7;
   }
   
-
-  console.log(total / 7);
+  let average = total / scores.length;
+  return average;
 }
-getAverage(myScores);
-getAverage(yourScores);
+let myAverage = getAverage(myScores);
+console.log(myAverage.toFixed(2));
+let yourAverage = getAverage(yourScores);
+console.log(yourAverage.toFixed(2));
